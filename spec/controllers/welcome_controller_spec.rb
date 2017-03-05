5296c061726cd576434179d2c0a54ab457cac34d
Ashwin Khurana <ashwin@ashwinkhurana.com>
2017-03-04 18:30:34 -0800

    assignment-13-testing: complete

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 RSpec.describe WelcomeController, type: :controller do
   describe "GET index" do
     it "renders the index template" do
-
       get :index
-
       expect(response).to render_template("index")
     end
   end
@@ -15,6 +13,13 @@ RSpec.describe WelcomeController, type: :controller do
       get :about
       expect(response).to render_template("about")
     end
-  end  
+  end
+
+  describe "GET about" do
+    it "renders the about template" do
+      get :faq
+      expect(response).to render_template("faq")
+    end
+  end
 
 end
